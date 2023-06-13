bc564c9e0ed765ae44e229bf3e32de29b274def1
Abdelaaziz  SABRI <116524839+sabri-abdelaaziz@users.noreply.github.com>
2023-06-13 18:17:09 +0100

    add products

--- wagazistore/views.py
+++ wagazistore/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+import pyodbc
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from .models import Product
 # Create your views here.
+
+
+
+
+
+def create(request):
+    
+    return redirect("index")
 
 def index(request):
     return render(request,"index.html")
@@ -11,11 +22,14 @@ def registre(request):
 def about(request):
     return render(request,"about.html")
 
-def cart(request):
-    return render(request,"cart.html")
-
 def contact(request):
     return render(request,"contact.html")
 
+def cart(request):
+    return render(request,"cart.html")
+
 def shop(request):
-    return render(request,"shop.html")
+    products = Product.objects.all()
+    return render(request,"shop.html",{'products': products})
+
+
